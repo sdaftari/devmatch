b34a96e29d5ed6465b70ff49d4cf7ec96780c8fd
shrutidaftari <shrutidaftari@gmail.com>
2016-03-06 23:21:15 +0000

    Added before filters to check correct users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,28 +1,41 @@
 class ProfilesController < ApplicationController
-    def new
-        # form where a user can fill out their own profile.
-        @user = User.find( params[:user_id] )
-        @profile = Profile.new
+  before_action :authenticate_user!
+  before_action :only_current_user
+  def new
+    # form where a user can fill out their own profile.
+    @user = User.find( params[:user_id] )
+    @profile = Profile.new
+  end
+  def create 
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile(profile_params)
+    if @profile.save
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :new
     end
-    
-    def create 
-        @user = User.find( params[:user_id] )
-        @profile = @user.build_profile(profile_params)
-        if @profile.save
-          flash[:success] = "Profile Updated!"
-          redirect_to user_path( params[:user_id] )
-        else
-          render action: :new
-        end
+  end
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+  end
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :edit
     end
-    
-    def edit
-      @user = User.find( params[:user_id] )
-      @profile = @user.profile
-    end
-    
-    private
+  end
+  private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
