36b19ec3c8bb12663f394108395c1e1a20cf31ce
shrutidaftari <shrutidaftari@gmail.com>
2016-03-06 07:15:06 +0000

    Added profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+  has_one :profile
   belongs_to :plan
   attr_accessor :stripe_card_token
   def save_with_payment
